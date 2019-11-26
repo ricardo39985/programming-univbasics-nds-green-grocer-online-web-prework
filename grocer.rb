9ff9bb31dbcc87fe9bf70463896f84d351bda007
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-26 23:19:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,36 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  pp cart
+  results = []
+for i in cart
+  coupon_counter = 0
+  first_hash = {}
+  w_coupon_hash = {}
+  while coupon_counter < coupons.length
+    if i[:item] == coupons[coupon_counter][:item]
+      first_hash[:item] = i[:item]
+      first_hash[:price] = i[:price]
+      first_hash[:clearance] = true
+      first_hash[:count] = i[:price].to_f % coupons[coupon_counter][:num].to_f
+
+    else
+      first_hash[:item] = i[:item]
+      first_hash[:price] = i[:price]
+      first_hash[:clearance] = false
+      first_hash[:count] = i[:count]
+    end
+    if i[:item] == coupons[coupon_counter][:item]
+    w_coupon_hash[:item] = i[:item] + " W/COUPON"
+      w_coupon_hash[:price] = coupons[coupon_counter][:cost].to_f / coupons[coupon_counter][:num].to_f
+      w_coupon_hash[:clearance] = true
+      w_coupon_hash[:count] = i[:count] -(i[:count] % coupons[coupon_counter][:num])
+    end
+    results.push(first_hash)
+    results.push(w_coupon_hash)
+    coupon_counter +=1
+  end
+end
+results
 end
 
 def apply_clearance(cart)
